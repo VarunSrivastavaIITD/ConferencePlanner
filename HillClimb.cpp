9756ee0058689a0db7da320d80872a0e849de3d9
Madhur Singal <madhursingal08@gmail.com>
2018-08-28 20:37:26 +0530

    Add score functions

--- HillClimb.cpp
+++ HillClimb.cpp
@@ -6,20 +6,23 @@
 
 #include "HillClimb.h"
 
-HillClimb::HillClimb(double **matrix, int p, int t, int k)
+HillClimb::HillClimb(double **matrix, int p, int t, int k, int c)
 {
     distance_matrix = matrix;
     parallel_tracks = p;
     sessions_in_track = t;
     papers_in_session = k;
+    trade_of_coefficient = c;
+
 }
 
-HillClimb::HillClimb(double **matrix, int p, int t, int k, State state)
+HillClimb::HillClimb(double **matrix, int p, int t, int k, int c, State state)
 {
     distance_matrix = matrix;
     parallel_tracks = p;
     sessions_in_track = t;
     papers_in_session = k;
+    trade_of_coefficient = c;
     initial_state = state;
 }
 
@@ -64,7 +67,48 @@ void HillClimb::random_initialize()
 {
 }
 
-double HillClimb::score_increment(State state1, State state2) const
+void HillClimb::updateState(int index_a, int index_b, State state){
+    int temp = state[index_a];
+    int session_seq_a = (index_a + papers_in_session)/papers_in_session - 1;
+    int session_seq_b = (index_b + papers_in_session)/papers_in_session - 1;
+    state[index_a] = state[index_b];
+    state[index_b] = temp;
+    session_distance_matrix[a][session_seq_a] += distance_matrix[a][b];
+    session_distance_matrix[a][session_seq_b] -= distance_matrix[a][b];
+    session_distance_matrix[b][session_seq_b] += distance_matrix[a][b];
+    session_distance_matrix[b][session_seq_a] -= distance_matrix[a][b];
+}
+
+double HillClimb::score_increment(int index_a, int a, int index_b, int b) const
 {
-    return 0;
+    double change = 0;
+    int session_seq_a = (index_a + papers_in_session)/papers_in_session - 1;
+    int session_seq_b = (index_b + papers_in_session)/papers_in_session - 1;
+    int papers_in_time_slot = papers_in_session*parallel_tracks;
+    int time_slot_a = (index_a+papers_in_time_slot)/(papers_in_time_slot)-1;
+    int time_slot_b = (index_b+papers_in_time_slot)/(papers_in_time_slot)-1;
+    if(time_slot_a == time_slot_b)
+        change = (trade_of_coefficient+1)*(session_distance_matrix[a][session_seq_a] 
+                                          +session_distance_matrix[b][session_seq_b] 
+                                          -session_distance_matrix[a][session_seq_b]
+                                          -session_distance_matrix[b][session_seq_a]
+                                          -2*distance_matrix[a][b]);
+                    
+
+    else{
+        change = (trade_of_coefficient+1)*(session_distance_matrix[a][session_seq_a]
+                 +session_distance_matrix[b][session_seq_b]
+                 -session_distance_matrix[a][session_seq_b]
+                 -session_distance_matrix[b][session_seq_a]
+                 +2*distance_matrix[a][b]);
+
+        for(int i=0;i<p;++i)
+            change += trade_of_coefficient*(session_distance_matrix[a][time_slot_b*parallel_tracks+i]
+                                           +session_distance_matrix[b][time_slot_a*parallel_tracks+i]
+                                           -session_distance_matrix[a][time_slot_a*parallel_tracks+i]
+                                           -session_distance_matrix[b][time_slot_b*parallel_tracks+i]);
+        
+    }
+
+    return change;
 }
--- HillClimb.h
+++ HillClimb.h
@@ -19,6 +19,7 @@ class HillClimb
     int parallel_tracks;
     int sessions_in_track;
     int papers_in_session;
+    double trade_of_coefficient;
 
     vector<vector<double>> session_distance_matrix;
     State initial_state;
@@ -33,14 +34,17 @@ class HillClimb
 
   public:
     // Constructors
-    HillClimb(double **, int, int, int);
-    HillClimb(double **, int, int, int, State);
+    HillClimb(double **, int, int, int, int);
+    HillClimb(double **, int, int, int, int, State);
 
     // Main hill climb algorithm
     State hill_climb(bool);
 
-    // Increment in score when going from state 1 to state 2
-    double score_increment(State, State) const;
+    // Increment in score when going from state 1 to state 2 by single swap
+    double score_increment(int, int, int, int) const;
+
+    //Update state and session distance matrix after single swap
+    void update_state(int, int, int, int, State);
 };
 
 #endif
